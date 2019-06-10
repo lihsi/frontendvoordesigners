1d4282b512da60cdf06cf87fc4ed01f914b61cc4
lihsi <50340772+lihsi@users.noreply.github.com>
2019-06-10 22:28:50 +0200

    Add files via upload

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -1,19 +1,30 @@
 var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
-var section = document.querySelector('section');
-console.log("section",section);
-
-
-console.log("loadimagesmetXHR");
 var request = new XMLHttpRequest();
 request.open('get', uri);
 request.responseType = 'json';
 request.send();
 
 request.addEventListener("load", function(){
-  var data = request.response;
   console.log("request is geladen: ",request.response);
-  //er is data
-  //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+
+  var data = request.response;
+    
+  var main = document.querySelector("main");
+  for(var i = 0; i < data.length; i++)
+  {
+    var film = data[i];
+      
+    var titel = document.createElement("h2");
+    titel.textContent = film.title;
+    main.appendChild(titel);
+    
+    var simplePlot = document.createElement("p");
+    simplePlot.textContent = film.simple_plot;
+    main.appendChild(simplePlot);
+    
+    var filmCover = document.createElement("img");
+    filmCover.src = film.cover;
+    main.appendChild(filmCover);  
+  }
 });
